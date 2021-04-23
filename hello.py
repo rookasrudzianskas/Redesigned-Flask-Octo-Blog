85d96d67e23f35b760648c6be198aa798401f26c
Rokas <rookas.rudzenskas@gmail.com>
2021-04-23 17:51:39 +0300

    Rokas

--- hello.py
+++ hello.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 
 # Create a new route
-
 @app.route('/')
 def index():
     return "<h1>Hello World</h1>"
+
+
+# localhost:5000 🚀/user/rokas
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>Hello {} 🤜</h1>".format(name)
