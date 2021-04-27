05e2bad3af0f0324753134f26c6d0d2563c9b32a
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 13:55:44 +0300

    Rokas

--- hello.py
+++ hello.py
@@ -24,3 +24,16 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
+
+
+# custom errror pages
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
