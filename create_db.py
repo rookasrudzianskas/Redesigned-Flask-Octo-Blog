049d4e58ab13075fcafe4ee59363414e53a3b816
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 21:51:21 +0300

    Rokas

--- create_db.py
+++ create_db.py
@@ -8,7 +8,7 @@ mydb = mysql.connector.connect(
 
 my_cursor = mydb.cursor()
 
-my_cursor.execute("CREATE DATABASE my_users")
+my_cursor.execute("CREATE DATABASE users")
 
 my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
