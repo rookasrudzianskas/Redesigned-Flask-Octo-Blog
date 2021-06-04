5c81e5ae459ccb4808de31d65c70744fc90ecb73
Rokas <rookas.rudzenskas@gmail.com>
2021-06-04 18:36:13 +0300

    Rokas

--- hello.py
+++ hello.py
@@ -26,6 +26,21 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+# create a blog post model
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    # something like adds _ to the url
+    slug = db.Column(db.String(255))
+
+#     create a post form
+
+
+
 # JSON everything
 @app.route("/date")
 def get_current_date():
@@ -38,6 +53,8 @@ def get_current_date():
 
     return favourite_pizza
     # return {"Date": date.today()}
+
+
 # something
 
 
