d7a82c711def7867b7ffeb32f7425c13e9ce00fc
Rokas <rookas.rudzenskas@gmail.com>
2021-09-04 18:53:03 +0300

    Rokas

--- hello.py
+++ hello.py
@@ -117,7 +117,8 @@ def dashboard():
     else:
         return render_template("dashboard.html",
                                form=form,
-                               name_to_update=name_to_update)
+                               name_to_update=name_to_update,
+                               id=id)
     # return render_template("dashboard.html")
 
 
