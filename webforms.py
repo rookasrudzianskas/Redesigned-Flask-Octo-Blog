6269cb3143ad3b488948169e34346318fbf6991a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-11 09:56:08 +0300

    Rokas

--- webforms.py
+++ webforms.py
@@ -1,3 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
+
+from wtforms.widgets import TextArea
+
+
 # create a  login form
 
 class LoginForm(FlaskForm):
