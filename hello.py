f6eb6e1eb965f9ef4547da13160470f8c9507197
Rokas <rookas.rudzenskas@gmail.com>
2021-10-01 19:36:40 +0300

    Rokas

--- hello.py
+++ hello.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
-from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from datetime import date
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from wtforms.widgets import TextArea
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from webforms import LoginForm, PostForm, UserForm, PasswordForm, NameForm
 
